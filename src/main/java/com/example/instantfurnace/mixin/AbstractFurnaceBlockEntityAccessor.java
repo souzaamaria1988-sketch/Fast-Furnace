2547a0cfffc2d7d03addf03a3fd66d2abebc6fba
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-26 16:09:23 -0300

    Refactor accessors for cook time and burn time

--- src/main/java/com/example/instantfurnace/mixin/AbstractFurnaceBlockEntityAccessor.java
+++ src/main/java/com/example/instantfurnace/mixin/AbstractFurnaceBlockEntityAccessor.java
@@ -7,15 +7,12 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(AbstractFurnaceBlockEntity.class)
 public interface AbstractFurnaceBlockEntityAccessor {
 
-    // field_11989 = cookTime
-    @Accessor(value = "field_11989", remap = false)
+    @Accessor("cookTime")
     void instantFurnace$setCookTime(int value);
 
-    // field_11988 = cookTimeTotal
-    @Accessor(value = "field_11988", remap = false)
+    @Accessor("cookTimeTotal")
     void instantFurnace$setCookTimeTotal(int value);
 
-    // field_11981 = burnTime (combustível)
-    @Accessor(value = "field_11981", remap = false)
-    void instantFurnace$setBurnTime(int value);
+    @Accessor("litTime")
+    void instantFurnace$setLitTime(int value);
 }
