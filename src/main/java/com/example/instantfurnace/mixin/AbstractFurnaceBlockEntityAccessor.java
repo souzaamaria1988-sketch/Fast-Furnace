18fff847640ea975f1e9d7c42af1a57561a1fc99
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-27 15:29:46 -0300

    Refactor Accessor annotations in AbstractFurnaceBlockEntityAccessor

--- src/main/java/com/example/instantfurnace/mixin/AbstractFurnaceBlockEntityAccessor.java
+++ src/main/java/com/example/instantfurnace/mixin/AbstractFurnaceBlockEntityAccessor.java
@@ -7,12 +7,12 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(AbstractFurnaceBlockEntity.class)
 public interface AbstractFurnaceBlockEntityAccessor {
 
-    @Accessor(value = "cookTime", aliases = "field_11989")
+    @Accessor("cookTime")
     void instantFurnace$setCookTime(int value);
 
-    @Accessor(value = "cookTimeTotal", aliases = "field_11988")
+    @Accessor("cookTimeTotal")
     void instantFurnace$setCookTimeTotal(int value);
 
-    @Accessor(value = "burnTime", aliases = "field_11981")
+    @Accessor("burnTime")
     void instantFurnace$setBurnTime(int value);
 }
